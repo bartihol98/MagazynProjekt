7ba89843ea80972b981bc78daeb50a7fb4d1c6e7
BartekHolysz <bartekholysz98@gmail.com>
2019-10-22 18:15:50 +0200

    tamto respo byl blad

---
+++ WirtualnyMagazyn/UserName.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WirtualnyMagazyn
+{
+    public class UserName
+    {
+        private string LoginName = "";
+        public string loginName
+        {
+            get { return LoginName; }
+            set { LoginName = value; }
+        }
+
+        private int userLevel = 0;
+        public int Userlevel
+        {
+            get { return userLevel; }
+            set
+            {
+                userLevel = value;
+            }
+        }
+
+    }
+}
